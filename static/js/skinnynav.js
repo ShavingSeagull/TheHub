dbf4a55498ac8f1d2faf8a31e05eed2f6fdad8a4
Scott Kipp <scott.ecfx@gmail.com>
2021-12-09 21:00:02 +0000

    Jigged the navbar CSS, JS and layout

--- static/js/skinnynav.js
+++ static/js/skinnynav.js
@@ -1,6 +1,49 @@
 /* jshint esversion: 6 */
 const SKINNYNAV = $('#skinny-nav');
+const SKINNYPROFILE = $('.skinny-nav-profile');
+const SKINNYLISTHIDDENELEMS = $('#skinny-nav-list').children('li').children('a').children('p');
+const BURGERMENU = $('#burger-menu');
 
+function hideElements() {
+    // OVERLAY.addClass('d-none');
+    SKINNYLISTHIDDENELEMS.addClass('d-none');
+    SKINNYPROFILE.addClass('d-none');
+}
+
+function showElements() {
+    SKINNYLISTHIDDENELEMS.removeClass('d-none');
+    SKINNYPROFILE.removeClass('d-none');
+}
+
+function expandMobileSkinny() {
+    SKINNYNAV.css('width', '250px');
+    // OVERLAY.removeClass('d-none');
+}
+
+function expandDesktopSkinny() {
+    SKINNYNAV.css('width', '270px');
+    // OVERLAY.removeClass('d-none');
+}
+
+BURGERMENU.click(function(){
+    if ($(window).width() < 992) {
+        if (SKINNYNAV.width() == 50) {
+            expandMobileSkinny();
+            setTimeout(showElements, 380);
+        } else {
+            SKINNYNAV.css('width', '50px');
+            hideElements();
+        }
+    } else {
+        if (SKINNYNAV.width() == 70) {
+            expandDesktopSkinny();
+            setTimeout(showElements, 440);
+        } else {
+            SKINNYNAV.css('width', '70px');
+            hideElements();
+        }
+    }
+})
 
 // Resize method is needed to prevent the media queries failing to fire if the CSS has been set via the click
 // events above. Only applies to windows being actively changed in size - fine either way on initial page load. 

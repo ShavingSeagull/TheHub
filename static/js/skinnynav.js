923c2385f3444d3ee64c83cec7e96298491d35a7
Scott Kipp <scott.ecfx@gmail.com>
2021-12-10 20:21:44 +0000

    Added nav overlay and started top bar

--- static/js/skinnynav.js
+++ static/js/skinnynav.js
@@ -1,59 +1,71 @@
 /* jshint esversion: 6 */
-const SKINNYNAV = $('#skinny-nav');
-const SKINNYPROFILE = $('.skinny-nav-profile');
-const SKINNYLISTHIDDENELEMS = $('#skinny-nav-list').children('li').children('a').children('p');
+const SIDENAV = $('#sidenav');
+const NAVPROFILE = $('.sidenav-profile');
+const NAVLISTHIDDENELEMS = $('#sidenav-list').children('li').children('a').children('p');
 const BURGERMENU = $('#burger-menu');
+const OVERLAY = $('#sidenav-overlay');
 
 function hideElements() {
-    // OVERLAY.addClass('d-none');
-    SKINNYLISTHIDDENELEMS.addClass('d-none');
-    SKINNYPROFILE.addClass('d-none');
+    OVERLAY.addClass('d-none');
+    NAVLISTHIDDENELEMS.addClass('d-none');
+    NAVPROFILE.addClass('d-none');
 }
 
 function showElements() {
-    SKINNYLISTHIDDENELEMS.removeClass('d-none');
-    SKINNYPROFILE.removeClass('d-none');
+    NAVLISTHIDDENELEMS.removeClass('d-none');
+    NAVPROFILE.removeClass('d-none');
 }
 
 function expandMobileSkinny() {
-    SKINNYNAV.css('width', '250px');
-    // OVERLAY.removeClass('d-none');
+    SIDENAV.css('width', '250px');
+    OVERLAY.removeClass('d-none');
 }
 
 function expandDesktopSkinny() {
-    SKINNYNAV.css('width', '270px');
-    // OVERLAY.removeClass('d-none');
+    SIDENAV.css('width', '270px');
+    OVERLAY.removeClass('d-none');
 }
 
 BURGERMENU.click(function(){
     if ($(window).width() < 992) {
-        if (SKINNYNAV.width() == 50) {
+        if (SIDENAV.width() == 50) {
             expandMobileSkinny();
             setTimeout(showElements, 380);
         } else {
-            SKINNYNAV.css('width', '50px');
+            SIDENAV.css('width', '50px');
             hideElements();
         }
     } else {
-        if (SKINNYNAV.width() == 70) {
+        if (SIDENAV.width() == 70) {
             expandDesktopSkinny();
             setTimeout(showElements, 440);
         } else {
-            SKINNYNAV.css('width', '70px');
+            SIDENAV.css('width', '70px');
             hideElements();
         }
     }
-})
+});
+
+// Click listener to close the navbar if the overlay is clicked
+OVERLAY.click(function() {
+    if ($(window).width() < 992) {
+        SIDENAV.css('width', '50px');
+        hideElements();
+    } else {
+        SIDENAV.css('width', '70px');
+        hideElements();
+    }
+});
 
 // Resize method is needed to prevent the media queries failing to fire if the CSS has been set via the click
 // events above. Only applies to windows being actively changed in size - fine either way on initial page load. 
 // Unlikely to be required much, but a failsafe just in case.
 $(window).resize(function(){
     if ($(window).width() < 992) {
-        SKINNYNAV.css('width', '50px');
+        SIDENAV.css('width', '50px');
         hideElements();
     } else {
-        SKINNYNAV.css('width', '70px');
+        SIDENAV.css('width', '70px');
         hideElements();
     }
 });

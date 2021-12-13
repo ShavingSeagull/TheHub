e6c61430caa9884f2abbd98f0eed0f7b3def68e1
Scott Kipp <scott.ecfx@gmail.com>
2021-12-13 18:25:31 +0000

    Updated settings and main URLs for AllAuth

--- the_hub/settings.py
+++ the_hub/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [

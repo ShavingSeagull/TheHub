27958b49b8b07b607fbbcf41c9918320e91ce2dc
Scott Kipp <scott.ecfx@gmail.com>
2021-12-17 21:47:00 +0000

    Home page layout underway. Needs tweaking for responsivity

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,9 +1,10 @@
 /* jshint esversion: 6 */
 
-/* A small animation to render the main header first, then the subheader afterward.
-*  The speeds are adjusted slightly for the major breakpoints, to avoid animations being too fast.
-*/
-$(document).ready(function(){
+/**
+ * A small animation to render the main header first, then the subheader afterward.
+ * The speeds are adjusted slightly for the major breakpoints, to avoid animations being too fast.
+ */
+function animateHeaders() {
     if($(window).width() > 900) {
         $('#home-header').animate({
             width: '50%'
@@ -29,4 +30,24 @@ $(document).ready(function(){
             }, 1400)
         });
     }
+}
+
+// Calls the animation function only when the page is ready
+$(document).ready(function(){
+    animateHeaders();
+});
+
+/* Resets the widths and plays the animation again on active screen resizing.
+*  This is primarily to avoid the headers causing overflow when switched from large to small screens.
+*/
+$(window).resize(function(){
+    $('#home-header').css('width', '0');
+    $('#home-subheader').css('width', '0');
+    animateHeaders();
+});
+
+$('.home-card').hover(function(){
+    $(this).children('p').css('color', '#e84610');
+}, function(){
+    $(this).children('p').css('color', '#4a4a4f');
 });

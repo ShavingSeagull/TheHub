95a672093a1ce2e1554b57f3fa243e5b6bff75b7
Scott Kipp <scott.ecfx@gmail.com>
2022-02-09 17:51:14 +0000

    Updated authorize URL name for the Google API

--- documents/urls.py
+++ documents/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.document_list, name="document_list"),
-    path('authorize', views.authorize, name="tester"),
+    path('authorize', views.authorize, name="authorize"),
     path('oauth2callback', views.oauth2Callback, name="oauth2callback"),
     path('test_api_request', views.test_api_request, name="test_api_request"),
 ]

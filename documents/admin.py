f5fc5e87c100cb5d8f44f25693fdb69a195ef2dd
Scott Kipp <scott.ecfx@gmail.com>
2022-02-17 20:26:12 +0000

    Added a list view for the Category class in the admin file

--- documents/admin.py
+++ documents/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Category, Tag
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag)

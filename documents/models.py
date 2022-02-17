37cc77f804f611da2ad3cf3f2149470e9f183e9c
Scott Kipp <scott.ecfx@gmail.com>
2022-02-17 15:10:28 +0000

    Added Category and Tag models

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 
@@ -5,6 +6,10 @@ class Category(models.Model):
     """
     The model for the document categories.
     """
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
     name = models.CharField(max_length=20)
 
     def __str__(self):
@@ -17,12 +22,26 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=20)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
 
-class Document(models.Model):
-    """
-    The model for a Google Doc/Sheet that
-    holds all of its info.
-    """
+# class Document(models.Model):
+#     """
+#     The model for a Google Doc/Sheet that
+#     holds all of the info neccessary to store
+#     outside of Google. Most info will be retrieved
+#     from the document's metadata on Google. This is
+#     primarily for data not stored in the Google 
+#     metadata, such as tags and categories. Other fields
+#     are primarily for document identification.
+#     """
+#     doc_id = models.CharField(max_length=255, unique=True, null=False, blank=False)
+#     web_view_link = models.URLField(max_length=255, null=False, blank=False)
+#     title = models.CharField(max_length=100, null=False, blank=False)
+#     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='doc_category', null=True, blank=True)
+#     tags = models.ManyToManyField(Tag, related_name='doc_tags', null=True, blank=True)
+

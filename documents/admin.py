2a5d587662228f13627573fbff01824c1f74aa88
Scott Kipp <scott.ecfx@gmail.com>
2022-02-17 15:08:54 +0000

    Registered Category and Tag models to admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Tag
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)

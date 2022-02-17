9719ad17911071b259a725a0df680ad701d0867c
Scott Kipp <scott.ecfx@gmail.com>
2022-02-17 20:25:40 +0000

    Added some Meta to the Category model

--- documents/models.py
+++ documents/models.py
@@ -11,9 +11,13 @@ class Category(models.Model):
         ordering = ['name']
 
     name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Tag(models.Model):
@@ -27,21 +31,3 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class Document(models.Model):
-#     """
-#     The model for a Google Doc/Sheet that
-#     holds all of the info neccessary to store
-#     outside of Google. Most info will be retrieved
-#     from the document's metadata on Google. This is
-#     primarily for data not stored in the Google 
-#     metadata, such as tags and categories. Other fields
-#     are primarily for document identification.
-#     """
-#     doc_id = models.CharField(max_length=255, unique=True, null=False, blank=False)
-#     web_view_link = models.URLField(max_length=255, null=False, blank=False)
-#     title = models.CharField(max_length=100, null=False, blank=False)
-#     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='doc_category', null=True, blank=True)
-#     tags = models.ManyToManyField(Tag, related_name='doc_tags', null=True, blank=True)
-

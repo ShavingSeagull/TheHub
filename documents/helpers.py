fe59a12be93dfbde276b959a2106116a288f0a71
Scott Kipp <scott.ecfx@gmail.com>
2022-02-25 16:52:09 +0000

    Tweaked the tag_formatter function in the helper file

--- documents/helpers.py
+++ documents/helpers.py
@@ -2,13 +2,20 @@
 # Helper methods for the Documents app
 # ------------------------------------
 
-def tag_list_formatter(tag_string: str):
+def tags_formatter(tag_list: list, extra_tags: str) -> str:
     """
-    Splits a string of tags into a list.
-    Removes the whitespace from the list.
+    Google Drive API only allows strings as custom metadata.
+    This takes the list of checkbox tags and the string of extra
+    tags and combines them into one, space-separated, string.
+    The original set of extra tags, which come as one string,
+    are checked and built into a new string, in order to remove
+    any rogue punctuation the user may have entered. Numbers that
+    are part of the tag name and the spaces between them are preserved.
     """
-    tag_list = tag_string.split(',')
-    for x in range(len(tag_list)):
-        tag_list[x] = tag_list[x].strip()
+    formatted_extra_tags = ""
 
-    return tag_list
+    for char in extra_tags:
+        if char.isalnum() or char == ' ':
+            formatted_extra_tags += char
+    
+    return f"{' '.join(tag_list)} {formatted_extra_tags}".strip()

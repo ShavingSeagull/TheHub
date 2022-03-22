13bc387d3d81d3ea3df13b65732969e9e2d23084
Scott Kipp <scott.ecfx@gmail.com>
2022-03-22 21:00:58 +0000

    Removed the random secret key generation to prevent Google state errors

--- the_hub/settings.py
+++ the_hub/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-from django.core.management.utils import get_random_secret_key
 if os.path.exists("env.py"):
     import env
 
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEV" in os.environ

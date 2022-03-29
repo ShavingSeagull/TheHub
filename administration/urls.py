39765ac378021112d4a5da51a4f391608fb815fd
Scott Kipp <scott.ecfx@gmail.com>
2022-03-29 17:00:09 +0100

    Added category view URLs

--- administration/urls.py
+++ administration/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     admin_area, create_user, 
     edit_user, delete_user,
-    create_category,
+    create_category, edit_category,
+    delete_category,
     user_data_api
 )
 
@@ -12,5 +13,7 @@ urlpatterns = [
     path('edit-user', edit_user, name="edit_user"),
     path('delete_user', delete_user, name="delete_user"),
     path('create-category', create_category, name="create_category"),
+    path('edit-category', edit_category, name="edit_category"),
+    path('delete-category', delete_category, name="delete_category"),
     path('user-data-api/<str:username>', user_data_api, name="user_data_api"),
 ]
